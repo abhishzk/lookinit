f9629ca2d0b79608dcab5e4152cb1e8c422e0f4a
Abhishek Kumar <abhishzk.dev@gmail.com>
2025-03-30 03:03:39 +0100

    feat: add Lookinit blog with introductory post and enterprise inquiry form

---
+++ data/blogs.js
@@ -0,0 +1,24 @@
+const blogs = [
+    {
+      slug: "introducing-lookinit",
+      title: "Introducing Lookinit – A Smarter Way to Search",
+      description: "Discover Lookinit, the intuitive search experience that makes finding the right information effortless.",
+      image: "/images/bg.png",
+      date: "2025-03-29",
+      content: `
+      ## Welcome to Lookinit! 🚀
+
+      Lookinit is designed to help you search smarter. Stay tuned for updates!
+    `
+    },
+    {
+      slug: "how-lookinit-works",
+      title: "How Lookinit Works – Behind the Scenes",
+      description: "A deep dive into Lookinit’s technology and how it delivers fast, relevant search results.",
+      image: "/images/bgw.png",
+      date: "2025-04-05",
+    },
+  ];
+  
+  export default blogs;
+  
